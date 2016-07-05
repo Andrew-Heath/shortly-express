353af5ba62101286b583c8f47a323135d815359b
Andrew-Heath <aheath42@gmail.com>
2016-07-04 20:51:56 -0700

    Signs up new users, logs them in, possibly remembers sessions

--- public/client/signupView.js
+++ public/client/signupView.js
@@ -3,12 +3,35 @@ Shortly.signupView = Backbone.View.extend({
 
   template: Templates['signup'],
 
-  // events: {
-  //   'submit': 'signup'
-  // },
+  events: {
+    'submit': 'signup'
+  },
 
   render: function() {
     this.$el.html( this.template() );
     return this;
+  },
+
+  signup: function() {
+    var $username = this.$el.find('input #username');
+    var username = $username.val();
+
+    var $password = this.$el.find('input #password');
+    var password = $password.val();
+
+    $username.val('');
+    $password.val('');
+
+    var options = {
+      'method': 'POST',
+      'uri': 'http://127.0.0.1:4568/signup',
+      'json': {
+        'username': username,
+        'password': password
+      }
+    };
+    
   }
+
+
 });
